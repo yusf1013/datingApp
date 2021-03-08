ecd394f995eed45b2b4fe67819d93edaf24f261b
Yusuf <bsse1013@iit.du.ac.bd>
2021-03-09 01:34:35 +0600

    api skeleton

---
+++ HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace HelloWorld.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        // GET
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello nigga!");
+        }
+        
+        [HttpPost]
+        public IActionResult Post([FromBody] JObject payLoad)
+        {
+            return Ok(payLoad);
+        }
+    }
+}
---
+++ HelloWorld/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using HelloWorld.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelloWorld.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
